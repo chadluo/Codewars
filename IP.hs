f9556dc8f148b4eebe76d5da29ceed4370c5fbae
chadluo <i@yuki.rocks>
2016-03-10 21:02:24 +1100

    relatively naive solution

--- IP.hs
+++ IP.hs
@@ -1,9 +1,22 @@
-module IPv4 where
+module IPv4 (int32ToIP) where
 
-import           Data.Bits
+import           Data.Bits (bit, rotateR, (.&.), (.|.))
+import           Data.List (intersperse)
+-- import           Data.Word (Word32)
 import           Data.Int  (Int32)
 
 type IPString = String
 
 int32ToIP :: Int32 -> IPString
-int32ToIP int32 = show (int32 .&. (bit 255 `shiftL` 24))
+int32ToIP w = concat $ intersperse "." $ map (\(b,o) -> show $ (w .&. b) `rotateR` o) octets
+
+octets :: [(Int32, Int)]
+octets = [(bin [n..(n+7)], n) | n <- [24,16..0]]
+
+bin :: [Int] -> Int32
+bin = foldl1 (.|.) . map bit
+
+----
+
+test :: IPString
+test = int32ToIP 2149583361
