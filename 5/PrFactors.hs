bea84b4b760c1a039b30c128c9baddfdc64e43f7
chadluo <i@yuki.rocks>
2016-03-12 03:33:41 +1100

    still could not meet performance requirements
    planned to do some profiling but failed, and could not guess which part
    slows down the program.
    
    the problem is related to large integer factorization, or probably.
    expect an integer taking about 10~20s to run and profile, but this is
    hard to reach. some got factorized in a blink of an eye but others were
    slow enough to receive a SIGKILL. this difference depends on the
    magnitudes of factors, with integers made up of small prime factors
    collapse faster than those of large factors. probably try to compose
    integers of proper factors (about 1e4 ~ 1e5).

--- 5/PrFactors.hs
+++ 5/PrFactors.hs
@@ -1,22 +1,13 @@
-module Codewars.Kata.PrFactors where
+module Codewars.Kata.PrFactors (prime_factors) where
 
 import           Control.Arrow ((&&&))
 import           Data.List     (group, (\\))
 
 prime_factors :: Integer -> String
-prime_factors n = p'' $ map (head &&& length) ((group . factorise) n)
-
-p'' :: [(Integer, Int)] -> String
-p'' = foldl f "" where
-  f str (base, power) = str ++ "(" ++ show base ++
-   (if power == 1 then "" else "**" ++ show power) ++ ")"
+prime_factors = printFac . map (head &&& length) . group . factorise
 
 factorise :: Integer -> [Integer]
-factorise n = t ([2..(n `div` 2)]++ [n]) n
-
--- primes m = sieve [2..m] where
---   sieve (x:xs) = x : sieve (xs \\ [x,x+x..m])
---   sieve [] = []
+factorise n = t (2 : ([3, 5 .. (div n 2)] ++ [n])) n;
 
 t :: [Integer] -> Integer -> [Integer]
 t [] _ = []
@@ -24,3 +15,8 @@ t _ 1  = []
 t ps n = if r == 0 then h : t ps q else t (tail ps) n where
   h = head ps
   (q, r) = divMod n h
+
+printFac :: [(Integer, Int)] -> String
+printFac = foldl f "" where
+  f str (base, power) = str ++ "(" ++ show base ++
+   (if power == 1 then "" else "**" ++ show power) ++ ")"
