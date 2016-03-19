868d1588d28f4afe5886a4f772aafee41fc8d76d
chadluo <i@yuki.rocks>
2016-03-19 23:58:12 +1100

    refactor justify
    abstracted linify and spacify. spacify might be further improved if use
    fold

--- 4/justify.hs
+++ 4/justify.hs
@@ -1,30 +1,36 @@
 module TextAlignJustify where
 
-import           Data.List
-
 justify :: String -> Int -> String
-justify text width = init $ unlines $ c width (words text) [] []
+justify text width = (init . unlines . spacify width . linify width . words) text
 
-c :: Int -> [String] -> [String] -> [String] -> [String]
-c w []     tmp os = os ++ [unwords tmp]
-c w (t:ts) tmp os
-  | 1 + length t + l' tmp <= w = c w ts (tmp ++ [t]) os
-  | otherwise                  = c w (t:ts) [] (os ++ [unwords' (w - l'' tmp) tmp])
+linify :: Int -> [String] -> [[String]]
+linify w = l w [] where
+  l _ t []     = [t]
+  l w t (x:xs) | (length . unwords) (x:t) <= w = l w (t ++ [x]) xs
+               | otherwise                     = t : l w [] (x:xs)
 
-l' xs = length xs + (sum . map length) xs - 1
-l'' = (sum . map length)
+spacify :: Int -> [[String]] -> [String]
+spacify _ [x]    = [unwords x]
+spacify w (x:xs) = unwords' l x : spacify w xs where
+  l = w - (sum . map length) x
+  unwords' _ [y]    = y
+  unwords' w (y:ys) = y ++ replicate m ' ' ++ unwords' (w - m) ys where
+    m = w `div` l' + if w `mod` l' == 0 then 0 else 1
+    l' = length ys
 
-unwords' :: Int -> [String] -> String
-unwords' _ [x] = x
-unwords' width (t:ts) = t ++ (replicate x ' ') ++ unwords' (width - x) ts where
-  x = width `div` l + if width `mod` l == 0 then 0 else 1
-  l = length ts
-
---
+test  = putStrLn $ justify "123 45 6" 7
+test1 = putStrLn $ justify sample 30
+test2 = putStrLn $ justify sample 80
 
 sample :: String
-sample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor."
-
-test0 = putStrLn $ justify "123 45 6" 7
-test  = putStrLn $ justify sample 30
-test2 = putStrLn $ justify sample 80
+sample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum \
+\sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, \
+\at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a \
+\imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, \
+\nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum \
+\justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus \
+\felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit \
+\fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at \
+\tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit \
+\amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et \
+\dolor."
